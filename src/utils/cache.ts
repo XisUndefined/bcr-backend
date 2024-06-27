d58f0e2787ac0667361eaa3f5d67ab49478de4ff
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-06-27 21:16:22 +0700

    feat: add upstash redis

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,10 +1,6 @@
 import { Redis } from "ioredis";
 
-const redis = new Redis({
-  port: parseInt(process.env.REDIS_PORT as string),
-  host: process.env.REDIS_HOST,
-  db: parseInt(process.env.REDIS_DB as string),
-});
+const redis = new Redis(process.env.UPSTASH_REDIS_URL as string);
 
 export const setCache = async (key: string, value: string, ttl: number) => {
   await redis.set(key, value, "EX", ttl);
