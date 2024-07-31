237b07126d005434714706226fdf2a883566a68f
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-07-31 19:57:51 +0700

    feat: add options for cors

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,14 @@ const app: Express = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+    credentials: true,
+  })
+);
 
 const swaggerUiOptions = {
   defaultModelExpandDepth: -1,
